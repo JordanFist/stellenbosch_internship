5bf296da7e7428ca0297e2b5daa0982164d2091b
Mr JF Sandri <23242965@cs456342.cs.sun.ac.za>
2018-07-20 16:26:25 +0200

    ok

--- carcassonne/src/main/java/carcassonne/Main.java
+++ carcassonne/src/main/java/carcassonne/Main.java
@@ -1,34 +1,53 @@
 package carcassonne;
-import carcassonne.core.Data;
+
 import carcassonne.core.Deck;
 import carcassonne.core.Players;
+import carcassonne.core.Move;
+import carcassonne.core.setTile;
+import carcassonne.core.Client;
+import carcassonne.core.Card;
+import carcassonne.core.Player;
 
 public class Main {
+
 	public static void main(String[] args) {
-		Data data = new Data();
+
+		/* Initialisation of the game */
+	
 		Deck deck = new Deck();
-		Players players = new Players(2);
-		
+		Players players = new Players(3);
+		setTile board = new setTile();
+		Client client = new Client();
+		Move playerMove;
+		Player playerTurn = players.firstPlayer(players);
+		Card card;
 		int round = 1;
-		int card;
+
+		/* End of initialisation */
 		
-		/* Game Loop */
-		System.out.println(players.remainingPlayers(players));
-		//while (deck.deckIsEmpty(deck) == false && players.remainingPlayers(players) > 1) {
-			//System.out.println("test");
-			//card = deck.drawCard(deck);
-			/*if (isPlayable() == true) {
-				ask player his move
-				if (move valid) {
-					do
-				else
-					eject player
-				compute next player				
-				}
-			}
-			System.out.println(This card is unplayable);
-			*/
-		//}
+		/* Beginning of the game Loop */
+		
+		while (deck.deckIsEmpty(deck) == false && players.remainingPlayers(players) > 1) {
+			System.out.printf("================= ROUND : %d =================%n", round);
+			++round;
+			card = deck.drawCard(deck);
+
+			if (board.isPlayable(board, card) == true) {
+				//playerMove.askMove(card, playerTurn, playerMove);
+				playerMove = client.clientMove(board, card, playerTurn);
+				
+				if (board.validMove(board, playerMove) == true) {
+					board.addSetTile(board, playerMove);
+					System.out.println();
+					System.out.printf("player: %d\n\ncard: %s \t dir: %s \t x: %d \t y: %d \n\n", playerTurn.id, card.id, playerMove.dir, playerMove.onto.x, playerMove.onto.y);
+				} else
+					players.ejectPlayer(playerTurn);
+				playerTurn = players.computeNextPlayer(players, playerTurn); 	
+			} else 
+				System.out.println("This card is unplayable");
+		}
+
+		/* End of the game loop */
 		
 	}
 }
